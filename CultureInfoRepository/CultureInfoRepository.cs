a95514fd52098a77624d07e4fa219b0c10469de1
Sijmen Koffeman <koffemans@hotmail.com>
2018-05-25 17:43:50 +0100

    simple example using built in cultureinfos .net

---
+++ CultureInfoRepository/CultureInfoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CultureInfoRepository
+{
+    public class CultureInfoRepository
+    {
+        public CultureInfoRepository()
+        {
+
+        }
+
+        public CultureInfo[] CultureInfos
+        {
+            get
+            {
+                return CultureInfo.GetCultures(CultureTypes.AllCultures);
+            }
+
+        }
+    }
+}
--- CultureInfos/Program.cs
+++ CultureInfos/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CultureInfoRepository;
+using System;
+using System.Globalization;
+using System.Linq;
 
 namespace CultureInfos
 {
@@ -6,7 +9,37 @@ namespace CultureInfos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cir = new CultureInfoRepository.CultureInfoRepository();
+            DutchCultures(cir);
+            PortugueseCultures(cir);
+            Console.ReadLine();
+        }
+
+        private static void DutchCultures(CultureInfoRepository.CultureInfoRepository cir)
+        {
+            var cis = cir.CultureInfos.Where(c => c.EnglishName.ToLower().StartsWith("dutch"));
+
+            foreach (var ci in cis)
+            {
+                WriteInfo(ci);
+            }
+        }
+
+        private static void PortugueseCultures(CultureInfoRepository.CultureInfoRepository cir)
+        {
+            var cis = cir.CultureInfos.Where(c => c.EnglishName.ToLower().StartsWith("portuguese"));
+
+            foreach (var ci in cis)
+            {
+                WriteInfo(ci);
+            }
+        }
+
+
+        static private void WriteInfo(CultureInfo ci)
+        {
+            Console.WriteLine($"LCID: {ci.LCID}\n\rISO: {ci.TwoLetterISOLanguageName}\n\rNativeName: {ci.NativeName}\n\rName: {ci.Name}\n\rEnglishName: {ci.EnglishName}\n\rThreeLetterISO: {ci.ThreeLetterISOLanguageName}\n\r");
+
         }
     }
 }
