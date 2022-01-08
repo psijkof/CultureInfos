371130076fee0b8b5480a57e80c163b5fda3b00c
Sijmen Koffeman <koffemans@hotmail.com>
2022-01-08 13:26:20 +0000

    made simple comparission possible

--- CultureInfos/Program.cs
+++ CultureInfos/Program.cs
@@ -1,22 +1,14 @@
-﻿using CultureInfoRepository;
-using System;
+﻿using System;
 using System.Globalization;
 using System.Linq;
 
 namespace CultureInfos
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            var cir = new CultureInfoRepository.CultureInfoRepository();
-            DutchCultures(cir);
-            PortugueseCultures(cir);
-            Console.ReadLine();
-        }
-
         private static void DutchCultures(CultureInfoRepository.CultureInfoRepository cir)
         {
+            Console.WriteLine("-----DUTCH------");
             var cis = cir.CultureInfos.Where(c => c.EnglishName.ToLower().StartsWith("dutch"));
 
             foreach (var ci in cis)
@@ -25,8 +17,22 @@ namespace CultureInfos
             }
         }
 
+        private static void Main(string[] args)
+        {
+            var cir = new CultureInfoRepository.CultureInfoRepository();
+
+            //DutchCultures(cir);
+            //PortugueseCultures(cir);
+
+            WriteInfo(cir.CultureInfos.First((c) => c.Name == "nl"));
+            WriteInfo(cir.CultureInfos.First((c) => c.Name == "en"));
+
+            Console.ReadLine();
+        }
+
         private static void PortugueseCultures(CultureInfoRepository.CultureInfoRepository cir)
         {
+            Console.WriteLine("-----PORTUGUESE------");
             var cis = cir.CultureInfos.Where(c => c.EnglishName.ToLower().StartsWith("portuguese"));
 
             foreach (var ci in cis)
@@ -35,11 +41,17 @@ namespace CultureInfos
             }
         }
 
-
-        static private void WriteInfo(CultureInfo ci)
+        private static void WriteInfo(CultureInfo ci)
         {
-            Console.WriteLine($"LCID: {ci.LCID}\n\rISO: {ci.TwoLetterISOLanguageName}\n\rNativeName: {ci.NativeName}\n\rName: {ci.Name}\n\rEnglishName: {ci.EnglishName}\n\rThreeLetterISO: {ci.ThreeLetterISOLanguageName}\n\r");
-
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"LCID: {ci.LCID}");
+            Console.WriteLine($"ISO: {ci.TwoLetterISOLanguageName}");
+            Console.WriteLine($"NativeName: {ci.NativeName}");
+            Console.WriteLine($"Name: {ci.Name}");
+            Console.WriteLine($"EnglishName: {ci.EnglishName}");
+            Console.WriteLine($"ThreeLetterISO: {ci.ThreeLetterISOLanguageName}");
+            Console.WriteLine($"Number Format:{string.Format(ci, "{0:C}", 123987239.87)}");
+            Console.WriteLine();
         }
     }
 }
